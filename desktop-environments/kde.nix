f05988c3f25c2223d2a30c398f94030e89e19b02
ilies <benyahia.ilies@gmail.com>
2025-09-29 14:36:12 +0200

    fix missing de module for amelie and tweaks on nvidia module for trying to avoid some wake up from sleep problems on our pcs

--- desktop-environments/kde.nix
+++ desktop-environments/kde.nix
@@ -6,4 +6,8 @@
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  networking.networkmanager.enable = true;
+  networking.wireless.iwd.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
+
 }
--- ilies-pc/nvidia.nix
+++ ilies-pc/nvidia.nix
@@ -18,7 +18,7 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
@@ -30,14 +30,14 @@
     # supported GPUs is at: 
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
+   	# accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
 }
