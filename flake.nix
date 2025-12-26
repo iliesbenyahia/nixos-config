0f8ebc8173bc773390a4c9b039ecb39d5bf07bbb
ilies <benyahia.ilies@gmail.com>
2025-12-26 18:42:43 +0100

    upgrade 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     lanzaboote = {
