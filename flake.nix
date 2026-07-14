ea9c151a19637cb3c80e88d293aae8b26033985d
iliesbenyahia <benyahia.ilies@gmail.com>
2026-07-14 13:41:16 +0200

    bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     lanzaboote = {
@@ -17,7 +17,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
